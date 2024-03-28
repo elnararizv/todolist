23422278d6277377c777c8e2e99e7c463a806cd2
elnara002 <6fdtxsg@code.edu.az>
2024-03-28 23:29:15 +0400

    to do list

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+// $(document).ready(function() {
+//     $("button").click(function() {
+//             $("ul").append("<li>" + $('#addList').val() + "</li>");
+//             $('#addList').val('');
+        
+//     });
+  
+
+
+
+let input = document.querySelector('.inp');
+let ul = document.querySelector('.ul-list');
+let addBtn = document.querySelector('.add-btn');
+ 
+addBtn.addEventListener('click', function () {
+    if(input.value!=''){
+         ul.innerHTML += `<li class="list-item">
+    <div class="d-flex justify-content-between mt-3 px-2 py-1 rounded">
+        <p class="text-danger">${input.value}</p>
+        <button class="btn delete-btn btn-danger"><i class="las la-trash-alt"></i></button>
+    </div>
+</li>`;
+    input.value = '';
+    }
+   
+
+    let deleteBtns = document.querySelectorAll('.delete-btn'); 
+    deleteBtns.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            let listItem = this.closest('li');
+            listItem.parentNode.removeChild(listItem);
+        });
+    });
+});
